2d726f23d4de7780e7cc958478d41ad246fdd1ed
DanilJK <fetisovdanil2001@gmail.com>
2021-01-22 09:36:41 +0200

    Change __dirname to process.cwd()

--- config/index.js
+++ config/index.js
@@ -1,15 +1,17 @@
 'use strict';
+
 require('dotenv').config();
 const fs = require('fs');
 const path = require('path');
 
 const config = {};
-const basePath = path.join(__dirname, 'components');
+const APP_PATH = process.cwd();
+const CONFIG_PATH = path.join(APP_PATH, 'config');
+const basePath = path.join(CONFIG_PATH, 'components');
 
-// Require all the files from the components folder and add the imported to a unique configuration object
 fs.readdirSync(basePath).forEach(file => {
-	const componentConfig = require(path.join(basePath, file));
-	Object.assign(config, componentConfig);
+  const componentConfig = require(path.join(basePath, file));
+  Object.assign(config, componentConfig);
 });
 
 module.exports = config;
